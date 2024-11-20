9e877f4db908a7c6c62b36d5c7e3dac38a6ca3b4
Pavel Petkov <elias5247698@abv.bg>
2024-11-20 18:15:36 +0200

    refactoring and make it class based

--- main.py
+++ main.py
@@ -1,73 +1,81 @@
 import math
 import pygame
 from pytmx.util_pygame import load_pygame
-
-pygame.init()
-display_info = pygame.display.Info()
-native_width, native_height = display_info.current_w, display_info.current_h
-tile_width = 16
-tile_height = 16
-VIEW_PORT_TILES_W = 32
-VIEW_PORT_TILES_H = 32
-current_max_dim = min(native_height, native_width)
-scale_factor = math.ceil(current_max_dim / tile_width / VIEW_PORT_TILES_W)
-screen = pygame.display.set_mode((current_max_dim - 1 / 6 * current_max_dim, current_max_dim - 1 / 6 * current_max_dim))
-tmx_data = load_pygame("src/tiles/level_1.tmx")
-current_start_pos_x = 100
-current_start_pos_y = 100
-print(scale_factor)
+from settings import TILE_WIDTH, TILE_HEIGHT, VIEW_PORT_TILES_W, VIEW_PORT_TILES_H
 
 
+class Game:
+    def __init__(self):
+        pygame.init()
+        current_max_dim = self.get_max_dimension()
+        self.screen = pygame.display.set_mode(
+            (current_max_dim - 1 / 6 * current_max_dim, current_max_dim - 1 / 6 * current_max_dim))
+        self.scale_factor = math.ceil(current_max_dim / TILE_WIDTH / VIEW_PORT_TILES_W)
+        self.tmx_data = load_pygame("src/tiles/level_1.tmx")
+        self.scale_grid()
+        self.current_start_pos_x = 100
+        self.current_start_pos_y = 100
 
-def get_tile_properties(x, y, layer):
-    if layer and hasattr(layer, 'tiles'):
-        gid = layer.data[y][x]
+    @staticmethod
+    def get_max_dimension():
+        display_info = pygame.display.Info()
+        native_width, native_height = display_info.current_w, display_info.current_h
+        current_max_dim = min(native_height, native_width)
+        return current_max_dim
 
-        properties = tmx_data.get_tile_properties_by_gid(gid)
-        return properties
-    return None
+    def scale_grid(self):
+        for gid, image in enumerate(self.tmx_data.images):
+            if image is not None:
+                new_width = image.get_width() * self.scale_factor
+                new_height = image.get_height() * self.scale_factor
+                scaled_image = pygame.transform.scale(image, (new_width, new_height))
+                self.tmx_data.images[gid] = scaled_image
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            self.update()
+            pygame.display.update()
+            pygame.display.flip()
+
+    def move_player(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.current_start_pos_x = self.current_start_pos_x - 0.7
+        if keys[pygame.K_RIGHT]:
+            self.current_start_pos_x = self.current_start_pos_x + 0.7
+        if keys[pygame.K_UP]:
+            self.current_start_pos_y = self.current_start_pos_y - 0.7
+        if keys[pygame.K_DOWN]:
+            self.current_start_pos_y = self.current_start_pos_y + 0.7
+
+    def render_map(self):
+        for layer in self.tmx_data.visible_layers:
+            if hasattr(layer, "tiles"):
+                for x, y, tile in layer.tiles():
+
+                    if self.current_start_pos_x - TILE_WIDTH <= x * TILE_WIDTH <= self.current_start_pos_x + VIEW_PORT_TILES_W * TILE_WIDTH + TILE_WIDTH \
+                            and self.current_start_pos_y - TILE_WIDTH <= y * TILE_HEIGHT <= self.current_start_pos_y + VIEW_PORT_TILES_H * TILE_HEIGHT + TILE_WIDTH:
+                        screen_x = (x * TILE_WIDTH - self.current_start_pos_x) * self.scale_factor
+                        screen_y = (y * TILE_HEIGHT - self.current_start_pos_y) * self.scale_factor
+                        self.screen.blit(tile, (screen_x, screen_y))
+
+    def update(self):
+        self.move_player()
+        self.render_map()
+
+    def get_tile_properties(self, x, y, layer):
+        if layer and hasattr(layer, 'tiles'):
+            gid = layer.data[y][x]
+
+            properties = self.tmx_data.get_tile_properties_by_gid(gid)
+            return properties
+        return None
 
 
-for gid, image in enumerate(tmx_data.images):
-    if image is not None:
-        new_width = image.get_width() * scale_factor
-        new_height = image.get_height() * scale_factor
-        scaled_image = pygame.transform.scale(image, (new_width, new_height))
-        tmx_data.images[gid] = scaled_image
-
-circle_surface = pygame.Surface((30 * 2, 30 * 2), pygame.SRCALPHA)
-pygame.draw.circle(circle_surface, (255, 0, 0), (5 * 2, 5 * 2), 5 * 2)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        current_start_pos_x = current_start_pos_x - 0.7
-    if keys[pygame.K_RIGHT]:
-        current_start_pos_x = current_start_pos_x + 0.7
-    if keys[pygame.K_UP]:
-        current_start_pos_y = current_start_pos_y - 0.7
-    if keys[pygame.K_DOWN]:
-        current_start_pos_y = current_start_pos_y + 0.7
-
-    center_x = screen.get_width() / 2
-    center_y = screen.get_width() / 2
-    for layer in tmx_data.visible_layers:
-        if hasattr(layer, "tiles"):
-            for x, y, tile in layer.tiles():
-
-                if current_start_pos_x - tile_width <= x * tile_width <= current_start_pos_x + VIEW_PORT_TILES_W * tile_width + tile_width \
-                        and current_start_pos_y - tile_width <= y * tile_height <= current_start_pos_y + VIEW_PORT_TILES_H * tile_height + tile_width:
-                    screen_x = (x * tile_width - current_start_pos_x) * scale_factor
-                    screen_y = (y * tile_height - current_start_pos_y) * scale_factor
-                    screen.blit(tile, (screen_x, screen_y))
-
-    screen.blit(circle_surface, (center_x, center_y))
-    screen.blit(circle_surface, (current_start_pos_x, current_start_pos_y))
-    pygame.display.update()
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == "__main__":
+    game = Game()
+    game.run()
