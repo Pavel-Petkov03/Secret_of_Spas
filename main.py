1cd85bbe8583608568a1f37f7a9006e8078ec995
Pavel Petkov <elias5247698@abv.bg>
2024-11-20 22:09:39 +0200

    changed tmx map and added collisions

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Game:
         self.scale_factor = math.ceil(current_max_dim / TILE_WIDTH / VIEW_PORT_TILES_W)
         self.tmx_data = load_pygame("src/tiles/level_1.tmx")
         self.scale_grid()
-        self.current_start_pos_x = 100
+        self.current_start_pos_x = 800
         self.current_start_pos_y = 100
         self.movement_speed = 0.7
 
@@ -44,20 +44,32 @@ class Game:
 
     def move_player(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a] and not self.collide_with_block(
+                self.current_start_pos_x - self.movement_speed,
+                self.current_start_pos_y):
             self.current_start_pos_x -= self.movement_speed
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d] and not self.collide_with_block(
+                self.current_start_pos_x + self.movement_speed,
+                self.current_start_pos_y):
             self.current_start_pos_x += self.movement_speed
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_w] and not self.collide_with_block(self.current_start_pos_x,
+                                                                                 self.current_start_pos_y - self.movement_speed):
             self.current_start_pos_y -= self.movement_speed
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s] and not self.collide_with_block(self.current_start_pos_x,
+                                                               self.current_start_pos_y + self.movement_speed):
             self.current_start_pos_y += self.movement_speed
+
+    def collide_with_block(self, new_x, new_y):
+        player_map_x = int((self.screen.get_width() / 2 / self.scale_factor + new_x) // TILE_WIDTH)
+        player_map_y = int((self.screen.get_height() / 2 / self.scale_factor + new_y) // TILE_HEIGHT)
+
+        result = self.get_tile_properties(player_map_x, player_map_y, self.tmx_data.layers[-1])
+        return result and result["is_block"]
 
     def render_map(self):
         for layer in self.tmx_data.visible_layers:
             if hasattr(layer, "tiles"):
                 for x, y, tile in layer.tiles():
-
                     if self.current_start_pos_x - TILE_WIDTH <= x * TILE_WIDTH <= self.current_start_pos_x + VIEW_PORT_TILES_W * TILE_WIDTH + TILE_WIDTH \
                             and self.current_start_pos_y - TILE_WIDTH <= y * TILE_HEIGHT <= self.current_start_pos_y + VIEW_PORT_TILES_H * TILE_HEIGHT + TILE_WIDTH:
                         screen_x = (x * TILE_WIDTH - self.current_start_pos_x) * self.scale_factor
@@ -67,11 +79,11 @@ class Game:
     def update(self):
         self.move_player()
         self.render_map()
+        pygame.draw.circle(self.screen, (255, 0, 0), (self.screen.get_width() / 2, self.screen.get_height() / 2), 20)
 
     def get_tile_properties(self, x, y, layer):
         if layer and hasattr(layer, 'tiles'):
             gid = layer.data[y][x]
-
             properties = self.tmx_data.get_tile_properties_by_gid(gid)
             return properties
         return None
