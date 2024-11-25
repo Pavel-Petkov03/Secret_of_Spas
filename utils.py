c918d17fb606b091e066ffb3b4c1275646c6098c
Pavel Petkov <elias5247698@abv.bg>
2024-11-26 01:58:35 +0200

    make valid resolutions for different devices

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 import pygame
-
+import functools
 pygame.init()
 
+VALID_RESOLUTIONS = [
+    1536,
+    1280,
+    1024,
+    768,
+    512,
+]
 
 def get_max_dimension():
     display_info = pygame.display.Info()
     native_width, native_height = display_info.current_w, display_info.current_h
     current_max_dim = min(native_height, native_width)
-    return current_max_dim
+    for resolution in VALID_RESOLUTIONS:
+        if resolution < current_max_dim:
+            return resolution
