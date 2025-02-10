2a5e35e3acdc02ac49bd03e2b5bd6a64ad97b368
Pavel Petkov <elias5247698@abv.bg>
2025-02-11 00:28:27 +0200

    addings fruits

--- item_inventory/settings.py
+++ item_inventory/settings.py
@@ -2,10 +2,10 @@ import random
 
 ITEM_DROP_FOR_MAP = {
     "first_level": {
-        "Grapes": 10,
-        "Sugar": 40,
-        "Yeast": 30,
-        "Water": 20
+        "Grapes": (10, ""),
+        "Sugar": (40, ""),
+        "Yeast": (30, ""),
+        "Water": (20, "")
     },
 }
 
@@ -13,5 +13,5 @@ ITEM_DROP_FOR_MAP = {
 def get_random_drop(dungeon_name):
     current_data = ITEM_DROP_FOR_MAP[dungeon_name]
     items = list(current_data.keys())
-    weights = list(current_data.values())
+    weights = [tup[0] for tup in current_data.values()]
     return random.choices(items, weights=weights, k=1)[0]
